bdb8639e365669331c12a5a37e6309b3dc5cc9b9
Talha-Jawed <jawedtalha0@gmail.com>
2019-11-19 13:20:11 +0500

    implement header

---
+++ src/component/Header.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import closeIcon from '../assets/closeWhite.png'
+import menuIcon from '../assets/menu.jpg'
+import './Style.css';
+
+
+
+class WebHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickMenu: false
+    };
+  }
+  render() {
+    const { clickMenu } = this.state
+    console.log('=====')
+    return (
+      <div className="App" >
+        <div className='logoName'>
+          <span className='headerName'>{"<"}</span>
+          <span id='namefont'>{"TaLha JaVed"}</span>
+          <span className='headerName'>{"/>"}</span>
+        </div>
+        <div className={clickMenu ? 'menuLine' : 'rightSide'} >
+          
+          <p className='headerName'>{"Skills"}</p>
+          <p className='headerName'>{"Projects"}</p>
+          <p className='headerName'>{"Open Source"}</p>
+          <p className='headerName'>{"Achivements"}</p>
+          <p className='headerName'>{"Contact Me"}</p>
+        </div>
+        {/* <div className='headerIcons'> */}
+        {
+          clickMenu ?
+            <img className='menuIcon' src={closeIcon} alt="Logo" onClick={() => this.setState({ clickMenu: false })} />
+            :
+            <img className='menuIcon' src={menuIcon} alt="Logo" onClick={() => this.setState({ clickMenu: true })} />
+        }
+        {/* </div> */}
+
+      </div>
+    );
+  }
+}
+
+export default WebHeader;
