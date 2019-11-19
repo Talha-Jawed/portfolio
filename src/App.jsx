21172ad53b5aeba71aeb31391b46b637e370686d
Talha-Jawed <jawedtalha0@gmail.com>
2019-11-19 13:19:44 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './component/Header'
+
+function App() {
+  return (
+    <div className='main'>
+
+      <Header />
+    </div>
+  );
+}
+
+export default App;
