34d5beafbfddb596d8519c77ed8ce73980645ef6
Talha-Jawed <jawedtalha0@gmail.com>
2019-11-20 17:12:36 +0500

    implement skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './component/Header'
+import Skill from './component/Skill';
 
 function App() {
   return (
     <div className='main'>
 
       <Header />
+      <Skill />
     </div>
   );
 }
