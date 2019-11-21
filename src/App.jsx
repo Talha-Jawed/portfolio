7a9dda083ad9998ce9fb3edf09f107caa48bb95b
Talha-Jawed <jawedtalha0@gmail.com>
2019-11-21 19:13:36 +0500

    implement social icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css';
 import Header from './component/Header'
 import Skill from './component/Skill';
 import About from './component/About';
+import Social from './component/Social';
 
 function App() {
   return (
     <div className='main'>
+      <Social />
       <Header />
       <Skill />
       <About />
---
+++ src/component/Social.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import devTo from '../assets/devTo.png';
+import linkedIn from '../assets/Linkedin.png';
+import fb from '../assets/fb.png';
+import github from '../assets/github.png';
+import npm from '../assets/npm.png';
+import twitter from '../assets/twitter.png'
+import './Style.css';
+
+
+
+class Social extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    render() {
+        return (
+            <div className='social' >
+
+                <a href='https://www.linkedin.com/in/talha-javed-450491176/'>
+                    <img src={linkedIn} alt="linkedIn" className='socialIcon' />
+                </a>
+                <a href="https://twitter.com/talha_javed97">
+                    <img src={twitter} alt="twitter" className='socialIcon' />
+                </a>
+                <a href="https://dev.to/talhajawed">
+                    <img src={devTo} alt="devTo" className='socialIcon' />
+                </a>
+                <a href="https://www.facebook.com/talha.jawed.71">
+                    <img src={fb} alt="fb" className='socialIcon' />
+                </a>
+                <a href="https://github.com/Talha-Jawed">
+                    <img src={github} alt="github" className='socialIcon' />
+                </a>
+                <a href="https://www.npmjs.com/~talha-javed">
+                    <img src={npm} alt="npm" className='socialIcon' />
+                </a>
+
+            </div>
+        );
+    }
+}
+
+export default Social;
