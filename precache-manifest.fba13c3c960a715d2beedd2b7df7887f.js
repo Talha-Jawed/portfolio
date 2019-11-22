bebea6201c1a99cbcb14fd0cb3e5451691cc2199
Talha-Jawed <jawedtalha0@gmail.com>
2019-11-23 02:00:35 +0500

    Updates

---
+++ precache-manifest.fba13c3c960a715d2beedd2b7df7887f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e047d07bf290a47fecc03464f79264",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dc1a10460f963f04f785",
+    "url": "/portfolio/static/css/main.887cb753.chunk.css"
+  },
+  {
+    "revision": "bc8aecde9c40c2614c95",
+    "url": "/portfolio/static/js/2.47330fc7.chunk.js"
+  },
+  {
+    "revision": "dc1a10460f963f04f785",
+    "url": "/portfolio/static/js/main.053b6c45.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "9812fa9637a59c4dfc467dac2c331752",
+    "url": "/portfolio/static/media/bg.9812fa96.jpg"
+  },
+  {
+    "revision": "369cee7c217c815b62aa3916463f82c9",
+    "url": "/portfolio/static/media/closeWhite.369cee7c.png"
+  },
+  {
+    "revision": "84a29ffe04d5a9991b1ffc000df4781d",
+    "url": "/portfolio/static/media/cssJS.84a29ffe.png"
+  },
+  {
+    "revision": "e82e21e54417d2d06ea24d3ea270694f",
+    "url": "/portfolio/static/media/devTo.e82e21e5.png"
+  },
+  {
+    "revision": "1f2eb26abad3db91f5698a952572dc16",
+    "url": "/portfolio/static/media/imag.1f2eb26a.jpeg"
+  },
+  {
+    "revision": "36814c748978e0b31ecf007c66c7dcf5",
+    "url": "/portfolio/static/media/menu.36814c74.jpg"
+  },
+  {
+    "revision": "f6d35e5f77a37c9ed7bcdc1381ce9cd9",
+    "url": "/portfolio/static/media/mongoDB.f6d35e5f.png"
+  },
+  {
+    "revision": "cb04570a6cff29d07fac65cb17d0106e",
+    "url": "/portfolio/static/media/npm.cb04570a.png"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "/portfolio/static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "2768551da1180077958b615f1cd55068",
+    "url": "/portfolio/static/media/twitter.2768551d.png"
+  }
+]);
