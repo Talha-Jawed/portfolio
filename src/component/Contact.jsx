b255cf45e92ff5f1ffa733b056a266ae5725c47b
Talha-Jawed <jawedtalha0@gmail.com>
2019-12-28 00:10:30 +0500

    styling fixed

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -16,6 +16,7 @@ class Contact extends Component {
                 <em className='skillEM'>Have a question or want to work together?</em>
                 <div style={{ marginTop: '60px' }}>
                     <p className='contact'>{'WhatsApp: '}<span style={{ color: '#266cde' }}>+92 306 2709986</span></p>
+                    <p className='contact'>{'Skype ID: '}<span style={{ color: '#266cde' }}>talha.jawed97</span> </p>
                     <p className='contact'>{'Email: '}<span style={{ color: '#266cde' }}>jawedtalha0@gmail.com</span> </p>
                 </div>
                 <em className='made'>Made with ❤️ by Talha Javed</em>
