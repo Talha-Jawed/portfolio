1bce950c10769c78b35a14ad79e80201e4cf5052
Talha-Jawed <jawedtalha0@gmail.com>
2020-03-01 14:39:50 +0500

    Icon Fix

--- src/component/Social.jsx
+++ src/component/Social.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import devTo from '../assets/devTo.png';
-import linkedIn from '../assets/Linkedin.png';
+import linkedIn from '../assets/LinkedIn.png';
 import fb from '../assets/fb.png';
 import github from '../assets/github.png';
 import npm from '../assets/npm.png';
@@ -25,22 +25,22 @@ class Social extends Component {
 
                 <div className='social' style={props}>
 
-                    <a href='https://www.linkedin.com/in/talha-javed-450491176/' target="_blank">
+                    <a href='https://www.linkedin.com/in/talha-javed-450491176/' target="_blank" rel="noopener noreferrer">
                         <img src={linkedIn} alt="linkedIn" className='socialIcon' />
                     </a>
-                    <a href="https://twitter.com/talha_javed97" target="_blank">
+                    <a href="https://twitter.com/talha_javed97" target="_blank" rel="noopener noreferrer">
                         <img src={twitter} alt="twitter" className='socialIcon' />
                     </a>
-                    <a href="https://dev.to/talhajawed" target="_blank">
+                    <a href="https://dev.to/talhajawed" target="_blank" rel="noopener noreferrer">
                         <img src={devTo} alt="devTo" className='socialIcon' />
                     </a>
-                    <a href="https://www.facebook.com/talha.jawed.71" target="_blank">
+                    <a href="https://www.facebook.com/talha.jawed.71" target="_blank" rel="noopener noreferrer">
                         <img src={fb} alt="fb" className='socialIcon' />
                     </a>
-                    <a href="https://github.com/Talha-Jawed" target="_blank">
+                    <a href="https://github.com/Talha-Jawed" target="_blank" rel="noopener noreferrer">
                         <img src={github} alt="github" className='socialIcon' />
                     </a>
-                    <a href="https://www.npmjs.com/~talha-javed" target="_blank">
+                    <a href="https://www.npmjs.com/~talha-javed" target="_blank" rel="noopener noreferrer">
                         <img src={npm} alt="npm" className='socialIcon' />
                     </a>
 
